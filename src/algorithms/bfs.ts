7878a29ce13f84c37c7ec337cd9f867820c202d0
Abhinandan-KP <abhirop538@gmail.com>
2025-12-18 19:30:08 +0530

    Update bfs.ts

--- src/algorithms/bfs.ts
+++ src/algorithms/bfs.ts
@@ -1,7 +1,4 @@
-
-
 import { NodeType } from '@/types/Node';
-
 
 export const bfs = (
   grid: NodeType[][],
@@ -9,55 +6,42 @@ export const bfs = (
   endNode: NodeType
 ): NodeType[] => {
   const visitedNodesInOrder: NodeType[] = [];
-  
-
   const queue: NodeType[] = [];
 
   startNode.isVisited = true;
   startNode.distance = 0;
   queue.push(startNode);
 
-  
-  while (queue.length) {
+  while (queue.length > 0) {
     const currentNode = queue.shift()!;
     visitedNodesInOrder.push(currentNode);
 
-   
     if (currentNode === endNode) return visitedNodesInOrder;
 
     const neighbors = getUnvisitedNeighbors(currentNode, grid);
     
     for (const neighbor of neighbors) {
-     
       neighbor.isVisited = true;
-      
       neighbor.distance = currentNode.distance + 1;
-      
       neighbor.previousNode = currentNode;
-      
       queue.push(neighbor);
     }
   }
 
-  
   return visitedNodesInOrder;
 };
-
 
 const getUnvisitedNeighbors = (node: NodeType, grid: NodeType[][]): NodeType[] => {
   const neighbors: NodeType[] = [];
   const { row, col } = node;
 
- 
-  if (row > 0) neighbors.push(grid[row - 1][col]);               
-  if (row < grid.length - 1) neighbors.push(grid[row + 1][col]); 
-  if (col > 0) neighbors.push(grid[row][col - 1]);               
-  if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]); 
+  if (row > 0) neighbors.push(grid[row - 1][col]);
+  if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
+  if (col > 0) neighbors.push(grid[row][col - 1]);
+  if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
 
-  
   return neighbors.filter((neighbor) => !neighbor.isVisited && !neighbor.isWall);
 };
-
 
 export const getNodesInShortestPathOrder = (endNode: NodeType): NodeType[] => {
   const nodesInShortestPathOrder: NodeType[] = [];
